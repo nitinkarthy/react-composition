806afacc0b5957257526bbd56320ca63dbe8865e
Nitin Karthy <nitin.karthy@gmail.com>
2021-10-27 12:13:10 +0530

    Changing default titles

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Specialized</h2>
       <SpecializedRootComponent />
+      <h2>Composed</h2>
       <ComposedRootComponent />
+      <h2>Lifted</h2>
       <LiftedRootComponent />
     </div>
   );
